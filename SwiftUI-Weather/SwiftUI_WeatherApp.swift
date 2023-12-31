239d5f69e2aac7919b2f571a350f799c70cfee4a
arslan <iamarslannasr@gmail.com>
2023-12-31 16:05:52 +0500

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Muhammad Arslan Nasr on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
